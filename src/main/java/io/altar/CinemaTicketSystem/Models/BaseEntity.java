728ba69618d713b161d1c072af2f94df480f9640
SorayaSantos <soraya.ss_3@hotmail.com>
2018-12-17 20:33:50 +0000

    inicio projeto

--- src/main/java/io/altar/CinemaTicketSystem/Models/BaseEntity.java
+++ src/main/java/io/altar/CinemaTicketSystem/Models/BaseEntity.java
@@ -1,5 +1,18 @@
 package io.altar.CinemaTicketSystem.Models;
 
-public class BaseEntity {
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable  {
+	private static final long serialVersionUID=1L;
+	
+	private long id;
+	
+	public BaseEntity() {
+	}
+
+	public long getId() {
+		return id;
+	}
 
 }
+//This creates the id for all entities and have the functions to get the id's created
--- src/main/java/io/altar/CinemaTicketSystem/Models/Ticket.java
+++ src/main/java/io/altar/CinemaTicketSystem/Models/Ticket.java
@@ -1,5 +1,13 @@
 package io.altar.CinemaTicketSystem.Models;
 
-public class Ticket {
 
+public class Ticket extends BaseEntity{
+
+	//Attributes
+	private static final long serialVersionUID=1L;
+	private String nameOfTheMovie;
+	private String age;
+	private String genre;
+	private String actors;
+	private String 
 }
