014056b8932d2527504d549d27f23e452705551d
SorayaSantos <44404904+SorayaSantos@users.noreply.github.com>
2018-12-19 02:08:47 +0000

    created app class

---
+++ src/main/java/io/altar/CinemaTicketSystem/App.java
@@ -0,0 +1,9 @@
+package io.altar.CinemaTicketSystem;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/cinema")
+public class App extends Application {
+
+}
